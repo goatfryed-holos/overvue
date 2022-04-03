9b763c2a0f053368275424c38ae241e7fee08b09
Omar Sood <omar.sood@holos-supply.de>
2022-04-03 12:40:06 +0200

    extended cat tile

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,14 +1,33 @@
  <template>
    <section>
-    <div>Hi folks! ♥</div>
+     <div>
+       <h2>Look at my cat</h2>
+       <div>
+         <img src="https://cataas.com/cat/cute" alt="cat">
+       </div>
+       <p>Isn't it cute?</p>
+     </div>
    </section>
  </template>
 
- <style>
+ <style lang="scss">
  section {
    display: flex;
    align-items: center;
    justify-content: center;
    min-height: 50vh;
+   gap: 2em;
+
+   > div {
+     padding: 1em;
+     border: aquamarine solid;
+     border-radius: 0.5em;
+   }
+ }
+ img {
+   display: block;
+   object-fit: cover;
+   max-width: 20em;
+   max-height: 20em;
  }
  </style>
