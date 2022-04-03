a1014145b4af9679a5952558b1de68a0fb0d09f5
Omar Sood <omar.sood@holos-supply.de>
2022-04-03 12:32:14 +0200

    basic static component

---
+++ src/MyApp.vue
@@ -0,0 +1,14 @@
+ <template>
+   <section>
+    <div>Hi folks! ♥</div>
+   </section>
+ </template>
+
+ <style>
+ section {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   min-height: 50vh;
+ }
+ </style>
